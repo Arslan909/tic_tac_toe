9a9a8fb71b5fcd44ba984da1e679e594d721de60
Arslan909 <arslanswl909@gmail.com>
2023-03-30 22:30:53 +0500

    made changes in check_win() to return the winner and removed the check_board() and formatted file using clang-format

--- include/board.c
+++ include/board.c
@@ -1,91 +1,78 @@
-#include <stdio.h>
 #include "board.h"
+#include <stdio.h>
 
-void init_board(struct Board *board)
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            board->board[i][j] = EMPTY;
-        }
+void init_board(struct Board *board) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      board->board[i][j] = EMPTY;
     }
+  }
 }
 
-void print_board(struct Board *board)
-{
-    printf("\n");
-    printf("    ");
-    for (int col = 0; col < SIZE; col++)
-    {
-        printf("%d ", col);
+void print_board(struct Board *board) {
+  printf("\n");
+  printf("    ");
+  for (int col = 0; col < SIZE; col++) {
+    printf("%d ", col);
+  }
+  printf("\n");
+  printf("    ");
+  for (int col = 0; col < SIZE; col++) {
+    printf("--");
+  }
+  printf("\n");
+  for (int row = 0; row < SIZE; row++) {
+    printf("%d | ", row);
+    for (int col = 0; col < SIZE; col++) {
+      printf("%c ", board->board[row][col]);
     }
     printf("\n");
-    printf("    ");
-    for (int col = 0; col < SIZE; col++)
-    {
-        printf("--");
-    }
-    printf("\n");
-    for (int row = 0; row < SIZE; row++)
-    {
-        printf("%d | ", row);
-        for (int col = 0; col < SIZE; col++)
-        {
-            printf("%c ", board->board[row][col]);
-        }
-        printf("\n");
-    }
-    printf("\n");
+  }
+  printf("\n");
 }
 
 int check_draw(struct Board *board) // 0: no draw, 1: draw
 {
-    for (int row = 0; row < 3; row++)
-    {
-        for (int col = 0; col < 3; col++)
-        {
-            if (board->board[row][col] != 'X' && board->board[row][col] != 'O')
-            {
-                return 0;
-            }
-        }
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 3; col++) {
+      if (board->board[row][col] != 'X' && board->board[row][col] != 'O') {
+        return 0;
+      }
     }
-    return 1;
+  }
+  return 1;
 }
 
 int check_win(struct Board *board) // 0: no winner, 1: player wins
 {
-    // Check rows
-    for (int row = 0; row < 3; row++)
-    {
-        if (board->board[row][0] == board->turn && board->board[row][1] == board->turn && board->board[row][2] == board->turn)
-        {
-            return 1;
-        }
+  // Check rows
+  for (int row = 0; row < 3; row++) {
+    if (board->board[row][0] == board->turn &&
+        board->board[row][1] == board->turn &&
+        board->board[row][2] == board->turn) {
+      board->turn;
+      return board->turn;
     }
+  }
 
-    // Check columns
-    for (int col = 0; col < 3; col++)
-    {
-        if (board->board[0][col] == board->turn && board->board[1][col] == board->turn && board->board[2][col] == board->turn)
-        {
-            return 1;
-        }
-    }
-    // Check diagonals
-    if (board->board[0][0] == board->turn && board->board[1][1] == board->turn && board->board[2][2] == board->turn)
-    {
-        return 1;
-    }
-    if (board->board[0][2] == board->turn && board->board[1][1] == board->turn && board->board[2][0] == board->turn)
-    {
-        return 1;
-    }
-    return 0;
-}
 
-int check_board(struct Board *board) // 0: no winner, 1: X wins, 2: O wins, 3: draw
-{
-    return check_win(board) ? (board->turn == "X" ? 1 : 2) : (check_draw(board) ? 3 : 0);
+  // Check columns
+  for (int col = 0; col < 3; col++) {
+
+    if (board->board[0][col] == board->turn &&
+        board->board[1][col] == board->turn &&
+        board->board[2][col] == board->turn) {
+      return board->turn;
+    }
+  }
+  // Check diagonals
+  if (board->board[0][0] == board->turn && board->board[1][1] == board->turn &&
+      board->board[2][2] == board->turn) {
+    return board->turn;
+  }
+  if (board->board[0][2] == board->turn && board->board[1][1] == board->turn &&
+      board->board[2][0] == board->turn) {
+    return board->turn;
+  }
+  return 0;
 }
