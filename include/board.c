bde275d578f057a37a740a121b6eb43b1257866b
MTS999 <mtsprogrammer999@gmail.com>
2023-03-31 00:19:38 +0500

    use Clang-formatter

--- include/board.c
+++ include/board.c
@@ -1,52 +1,40 @@
-#include <stdio.h>
 #include "board.h"
+#include <stdio.h>
 
-void init_board(struct Board *board)
-{
-    for (int i = 0; i < SIZE; i++)
-    {
-        for (int j = 0; j < SIZE; j++)
-        {
-            board->board[i][j] = EMPTY;
-        }
+void init_board(struct Board *board) {
+  for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+      board->board[i][j] = EMPTY;
     }
+  }
 }
-void print_board(struct Board *board)
-{
-    printf("\n");
-    printf("    ");
-    for (int col = 0; col < 3; col++)
-    {
-        printf("%d ", col);
-    }
-    printf("\n");
-    printf("    ");
-    for (int col = 0; col < 3; col++)
-    {
-        printf("--");
-    }
-    printf("\n");
-    for (int row = 0; row < 3; row++)
-    {
-        printf("%d | ", row);
-        for (int col = 0; col < 3; col++)
-        {
-            printf("%c ", board->board[row][col]);
-        }
-        printf("\n");
+void print_board(struct Board *board) {
+  printf("\n");
+  printf("    ");
+  for (int col = 0; col < 3; col++) {
+    printf("%d ", col);
+  }
+  printf("\n");
+  printf("    ");
+  for (int col = 0; col < 3; col++) {
+    printf("--");
+  }
+  printf("\n");
+  for (int row = 0; row < 3; row++) {
+    printf("%d | ", row);
+    for (int col = 0; col < 3; col++) {
+      printf("%c ", board->board[row][col]);
     }
     printf("\n");
+  }
+  printf("\n");
 }
-void make_move(struct Board *board, int row, int col) // make move for current player
-{
-    board->board[row][col] = board->turn;
+void make_move(struct Board *board, int row, int col) {
+  board->board[row][col] = board->turn;
 }
-void turn_change(struct Board *board)
-{
-    board->turn = (board->turn == 'X') ? 'O' : 'X';
+void turn_change(struct Board *board) {
+  board->turn = (board->turn == 'X') ? 'O' : 'X';
 }
-int check_valid_move(char board[3][3], int row, int col)
-{
-   return (board[row][col] == 'X' || board[row][col] == 'O') ? 0 : 1;
-
+int check_valid_move(char board[3][3], int row, int col) {
+  return (board[row][col] == 'X' || board[row][col] == 'O') ? 0 : 1;
 }
--- include/board.h
+++ include/board.h
@@ -4,18 +4,19 @@
 #define SIZE 3
 #define EMPTY ' '
 
-struct Board {
+struct Board
+{
     char board[3][3];
-    char turn;   
+    char turn;
 };
 
 void init_board(struct Board *board);
 void print_board(struct Board *board);
-int check_board(struct Board *board); // 0: no winner, 1: X wins, 2: O wins, 3: draw
-int check_win(char board[3][3], char player); // 0: no winner, 1: player wins
-int check_draw(char board[3][3]); // 0: no draw, 1: draw
+int check_board(struct Board *board);                     // 0: no winner, 1: X wins, 2: O wins, 3: draw
+int check_win(char board[3][3], char player);             // 0: no winner, 1: player wins
+int check_draw(char board[3][3]);                         // 0: no draw, 1: draw
 int check_valid_move(char board[3][3], int row, int col); // 0: invalid move, 1: valid move
-void make_move(struct Board *board, int row, int col); // make move for current player
+void make_move(struct Board *board, int row, int col);    // make move for current player
 void turn_change(struct Board *board);
 
 #endif // BOARD_H
