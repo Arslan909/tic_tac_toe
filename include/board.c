31d3a37d5d2e64bcffe6a5fb57ff1e0d5428aa89
Arslan909 <arslanswl909@gmail.com>
2023-03-31 22:48:37 +0500

    created function to return the the current winner and functionality to check draw state of baord and used LLVM clang-format

--- include/board.c
+++ include/board.c
@@ -41,3 +41,47 @@ void turn_change(struct Board *board) {
 int check_valid_move(char board[3][3], int row, int col) {
   return (board[row][col] == 'X' || board[row][col] == 'O') ? 0 : 1;
 }
+
+int check_draw(struct Board *board) // 0: no draw, 1: draw
+{
+  for (int row = 0; row < 3; row++) {
+    for (int col = 0; col < 3; col++) {
+      if (board->board[row][col] != 'X' && board->board[row][col] != 'O') {
+        return 0;
+      }
+    }
+  }
+  return 1;
+}
+
+int check_win(struct Board *board) // 0: no winner, 1: player wins
+{
+  // Check rows
+  for (int row = 0; row < 3; row++) {
+    if (board->board[row][0] == board->turn &&
+        board->board[row][1] == board->turn &&
+        board->board[row][2] == board->turn) {
+      return board->turn;
+    }
+  }
+
+  // Check columns
+  for (int col = 0; col < 3; col++) {
+    if (board->board[0][col] == board->turn &&
+        board->board[1][col] == board->turn &&
+        board->board[2][col] == board->turn) {
+      return board->turn;
+    }
+  }
+
+  // Check diagonals
+  if (board->board[0][0] == board->turn && board->board[1][1] == board->turn &&
+      board->board[2][2] == board->turn) {
+    return board->turn;
+  }
+  if (board->board[0][2] == board->turn && board->board[1][1] == board->turn &&
+      board->board[2][0] == board->turn) {
+    return board->turn;
+  }
+  return 0;
+}
